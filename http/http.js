96224b8645b664317eff026c880b4c91a19f3396
Jason Ngo <greenavocado@protonmail.com>
2021-03-03 13:41:32 -0800

    feat: more explicit path

--- http/http.js
+++ http/http.js
@@ -28,7 +28,7 @@ app.use('*', function(req, res, next) {
 const wsProxy = createProxyMiddleware('/socket', {
     target: 'http://projects.jasonn.dev',
     router: {
-        '/ubc-cctweaked': 'http://localhost:5110'
+        '/socket/ubc-cctweaked': 'http://localhost:5110'
     },
     changeOrigin: true,
     ws: true
